165235d209951e2d1fb267d0756223a5aa16b318
zacaree <zacharem@gmail.com>
2019-03-17 15:22:11 -0500

    Solved Bitwise Operators. Now to optimize the code

--- JSFundamentals/Day6_Bitwise Operators.js
+++ JSFundamentals/Day6_Bitwise Operators.js
@@ -1,11 +1,21 @@
 // Work in progress!!!
 
-// [n, k] = [[5, 8, 2], [2, 5, 2]];
-const arr = ["5 2", "8 5", "2 2"];
-[n, k] = arr.map(set => set.split(" "));
+const numsInSequence = [5, 8, 2];
+const maxBitwiseAllowed = [2, 5, 2];
 
 function getMaxLessThanK(n, k) {
-  console.log(n, k);
+  let maxResult = 0;
+  // n is an integer.
+  // Iterate through a sequence of numbers starting at 1
+  // counting up to the given integer.
+  for (let i = 1; i <= n; i++) {
+    for (let j = i + 1; j <= n; j++) {
+      if ((i & j) > maxResult && (i & j) < k) {
+        maxResult = i & j;
+      }
+    }
+  }
+  return maxResult;
 }
 
-getMaxLessThanK(n, k);
+getMaxLessThanK();
