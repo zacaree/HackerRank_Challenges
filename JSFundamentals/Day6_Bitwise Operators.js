2ef3fb97f3d509ac3913923c0ef512115dde31bc
zacaree <zacharem@gmail.com>
2019-03-17 07:05:00 -0500

    Working through the problem

---
+++ JSFundamentals/Day6_Bitwise Operators.js
@@ -0,0 +1,11 @@
+// Work in progress!!!
+
+// [n, k] = [[5, 8, 2], [2, 5, 2]];
+const arr = ["5 2", "8 5", "2 2"];
+[n, k] = arr.map(set => set.split(" "));
+
+function getMaxLessThanK(n, k) {
+  console.log(n, k);
+}
+
+getMaxLessThanK(n, k);
