fbe4338cc5480674a85f0fd6da35bf9b5002bd38
Zac <zacaree@gmail.com>
2019-03-18 08:56:01 -0500

    Solved RegEx Challenge 1

--- JSFundamentals/Day7_RegEx.js
+++ JSFundamentals/Day7_RegEx.js
@@ -1,2 +1,17 @@
-// A work in progress...
-const re = /[aeiou]ab+c[aeiou]/;
+const s1 = "adeogjsketa"; //True
+const s2 = "adeogjskete"; //False
+
+//  ^ => first item matches:
+// () => stores matching value captured within
+// [aeiou] => matches any of the characters in the brackets
+// . => matches any character:
+// * => for 0 or more occurrances
+// \1 => matches to previously stored match.
+////// \2 would look for matched item stored 2 instances ago
+////// \3 would look for matched item stored 3 ago, etc
+// $ ensures that matched item is at end of the sequence
+
+const firstAndLast_SameVowel = /^([aeiou]).*\1$/;
+
+console.log(firstAndLast_SameVowel.test(s1));
+console.log(firstAndLast_SameVowel.test(s2));
