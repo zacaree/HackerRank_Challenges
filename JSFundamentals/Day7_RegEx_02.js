c9f4a054b817dc506f8fdbeea7193111e147a0f3
Zac <zacaree@gmail.com>
2019-03-18 16:29:17 -0500

    Working on RegEx challenge 2

---
+++ JSFundamentals/Day7_RegEx_02.js
@@ -0,0 +1,22 @@
+// WORK IN PROGRESS!!!
+// WORK IN PROGRESS!!!
+// WORK IN PROGRESS!!!
+
+// const prefixes = ^(["Mr."|"Mrs."|"Ms."|"Dr."|"Er."])
+
+//  ^ => first item matches:
+// () => stores matching value captured within
+// [aeiou] => matches any of the characters in the brackets
+// . => matches any character:
+// * => for 0 or more occurrances
+// \1 => matches to previously stored match.
+////// \2 would look for matched item stored 2 instances ago
+////// \3 would look for matched item stored 3 ago, etc
+// $ ensures that matched item is at end of the sequence
+
+const firstAndLast_SameVowel = /^([aeiou]).*\1$/;
+
+const firstAndLast_SameVowel = /^(Mr\.|Mrs\.|Ms\.|Dr\.|Er\.)/;
+
+console.log(firstAndLast_SameVowel.test(s1));
+console.log(firstAndLast_SameVowel.test(s2));
