d8faffae6d267001cc01a33ed8616b1d2f449a74
zacaree <zacharem@gmail.com>
2019-03-23 11:21:40 -0500

    Built a second solution for the button rotation challenge

--- JSFundamentals/Day8_Buttons Container/Solution_02/script.js
+++ JSFundamentals/Day8_Buttons Container/Solution_02/script.js
@@ -3,9 +3,13 @@ const ids = [1, 2, 3, 6, 9, 8, 7, 4];
 
 let btn5 = document.getElementById("btn5");
 
-btn5.onclick = function() {
+function rotateNums() {
+  // pop removes item from end of array and returns that item.
+  // unshift adds items to the beginning of an array.
   nums.unshift(nums.pop());
   for (i = 0; i <= 7; i++) {
     document.getElementById("btn" + ids[i]).innerHTML = nums[i];
   }
-};
+}
+
+btn5.addEventListener("click", rotateNums);
