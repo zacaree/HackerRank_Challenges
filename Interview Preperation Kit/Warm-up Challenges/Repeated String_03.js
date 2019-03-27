4e4c796ae838b9723668fbaba624fb604fb1eb2c
zacaree <zacharem@gmail.com>
2019-03-27 09:42:01 -0500

    Repeated strings - SOLVED!

--- Interview Preperation Kit/Warm-up Challenges/Repeated String_03.js
+++ Interview Preperation Kit/Warm-up Challenges/Repeated String_03.js
@@ -1,6 +1,8 @@
+// Initial test
 const s1 = "aba";
 const n1 = 10;
 
+// Huge n
 const s2 = "a";
 const n2 = 1000000000000;
 
@@ -8,35 +10,33 @@ const n2 = 1000000000000;
 const s3 = "bcd";
 const n3 = 15;
 
-// n is less than string
-const s4 = "abcdefghijklmnopqrstuvwxyz";
-const n4 = 6;
-
-// n is less than string and target is not included
-const s5 = "bcdefghijklmnopqrstuvwxyza";
-const n5 = 6;
-
-// Currently working to solve test 3
+// This solution passes all HackerRank tests in O(n);
 function repeatedString(s, n) {
-  const findTargetChars = string => string.match(/a/g).length;
+  const findTargetChars = string => {
+    const matches = string.match(/a/g);
+    return matches ? matches.length : 0;
+  };
 
+  // How many times does s fit into n?
   const wholeStringsInN = Math.floor(n / s.length);
+  // How many "a"s are in s? Multiply that by the number of whole strings in n.
   const targetCharsInWhole = findTargetChars(s) * wholeStringsInN;
 
+  // What part of the string gets chopped off to fit in n?
   const remainder = n % s.length;
   let remainingString;
   let targetCharsInRem = 0;
 
   if (remainder) {
     remainingString = s.substring(0, remainder);
+    // How many "a"s appear in the remaining string?
     targetCharsInRem = findTargetChars(remainingString);
   }
 
+  // Add the two together and return the value.
   return targetCharsInWhole + targetCharsInRem;
 }
 
 console.log(repeatedString(s1, n1));
 console.log(repeatedString(s2, n2));
 console.log(repeatedString(s3, n3));
-console.log(repeatedString(s4, n4));
-console.log(repeatedString(s5, n5));
