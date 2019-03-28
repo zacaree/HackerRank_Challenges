fb6f1b79994816ee291401409b180a9adbf0d859
zacaree <zacharem@gmail.com>
2019-03-27 22:11:00 -0500

    Working on challenge: New year chaos

--- Interview Preperation Kit/Arrays/New Year Chaos.js
+++ Interview Preperation Kit/Arrays/New Year Chaos.js
@@ -9,14 +9,22 @@ const q1 = [2, 5, 1, 3, 4];
 //
 
 function minimumBribes(q) {
+  let count = 0;
   for (let i = 1; i <= q.length; i++) {
-    if (q[i] === i) {
+    if (q[i - 1] === i) {
       console.log("👍");
-    } else if (i - q[i]) {
-      // wip - check this
+    } else if (q[i - 1] === i + 1) {
+      count++;
+      i = i += 1;
+    } else if (q[i - 1] === i + 2) {
+      count += 2;
+      i = i += 2;
+    } else {
+      count = "Too chaotic";
     }
   }
+  return count;
 }
 
 console.log(minimumBribes(q0));
-// console.log(minimumBribes(q1));
+console.log(minimumBribes(q1));
