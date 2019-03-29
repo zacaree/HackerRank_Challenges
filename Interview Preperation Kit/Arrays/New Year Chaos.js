f31d837edbb809aebe21fbfa5fcf4d254615a53f
Zac <zacaree@gmail.com>
2019-03-29 12:58:09 -0500

    Passed 8 tests. 4 tests timeout. Need to optimize.

--- Interview Preperation Kit/Arrays/New Year Chaos.js
+++ Interview Preperation Kit/Arrays/New Year Chaos.js
@@ -1,6 +1,7 @@
 // Test 0
 const q0a = [2, 1, 5, 3, 4];
 const q0b = [2, 5, 1, 3, 4];
+// const  = [1, 4, 0, 2, 3];
 // Expected output:
 // 3
 // Too chaotic
@@ -8,7 +9,6 @@ const q0b = [2, 5, 1, 3, 4];
 // Test 1
 const q1a = [5, 1, 2, 3, 7, 8, 6, 4];
 const q1b = [1, 2, 5, 3, 7, 8, 6, 4];
-const aaa = [1, 2, 3, 4, 5, 6, 7, 8];
 // Expected output:
 // Too chaotic
 // 7
@@ -22,31 +22,32 @@ const q2a = [1, 2, 5, 3, 4, 7, 8, 6];
 
 //
 
+// The key:
+// For each person `line[i]`, determine how many numbers
+// to the right of this person are less than this person `line[i]`.
+
 function minimumBribes(q) {
-  let count = 0;
+  let bribes = 0;
 
   // reduce each int by 1 making it easier to match up with i.
   const line = q.map(p => p - 1);
 
   for (let i = 0; i < line.length; i++) {
     if (line[i] > i + 2) {
-      count = "Too chaotic";
-    }
-    for (let j = 0; j < line.length; j++) {
-      // find a way to start looking after the specified number...
-      if (line[i] < line[j]) {
-        count++;
+      return "Too chaotic";
+    } else {
+      // Only make comparisons with numbers that occur to the right of i.
+      for (let j = i + 1; j < line.length; j++) {
+        if (line[i] > line[j]) bribes++;
       }
     }
   }
 
-  console.log(count);
+  return bribes;
 }
 
-// minimumBribes(q0a);
-minimumBribes(q0b);
-// minimumBribes(q1a);
-// minimumBribes(q1b);
-
-// key: count, How many times has this person been bribed?
-// for each num, how many numbers to the right of this num, are less than this num?
+console.log(minimumBribes(q0a));
+console.log(minimumBribes(q0b));
+console.log(minimumBribes(q1a));
+console.log(minimumBribes(q1b));
+console.log(minimumBribes(q2a));
