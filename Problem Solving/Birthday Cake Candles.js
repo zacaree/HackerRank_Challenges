1da5fa2b6072a9011cd1d4973236d5f95dc301f7
zacaree <zacharem@gmail.com>
2019-04-03 07:44:24 -0500

    Challenge in progress

---
+++ Problem Solving/Birthday Cake Candles.js
@@ -0,0 +1,20 @@
+// You are in charge of the cake for your niece's birthday and have decided
+// the cake will have one candle for each year of her total age.
+// When she blows out the candles, she’ll only be able to blow out the tallest ones.
+// Your task is to find out how many candles she can successfully blow out.
+// Highest numbers represent the tallest candles
+
+const candleHeights = [3, 2, 1, 3];
+
+// Expected output
+// 2
+
+//
+
+//
+
+function birthdayCakeCandles(ar) {
+  console.log(ar);
+}
+
+console.log(birthdayCakeCandles(candleHeights));
