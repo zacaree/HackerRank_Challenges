4146efdef5b072a663d5248526b382c8c553957d
zacaree <zacharem@gmail.com>
2019-04-08 08:01:33 -0500

    Completed challenge: Python Arithmetic Operators

---
+++ Python/Arithmetic Operators.py
@@ -0,0 +1,23 @@
+# Read two integers from STDIN and print three lines where:
+
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+
+# Sample input
+a = 3
+b = 2
+
+# Sample output
+# 5
+# 1
+# 6
+
+
+def arithmetic(a, b):
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
+
+arithmetic(a, b)
