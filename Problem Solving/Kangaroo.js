0e112ecf851f27b9f84671e4c927114d84cb5f1d
Zac <zacaree@gmail.com>
2019-04-29 12:17:48 -0500

    Working on Kangaroo

--- Problem Solving/Kangaroo.js
+++ Problem Solving/Kangaroo.js
@@ -1,27 +1,27 @@
 const k1 = [0, 3, 4, 2]; // YES
 const k2 = [0, 2, 5, 3]; // NO
+const k3 = [43, 2, 70, 2]; // NO
+const k4 = [2564, 5393, 5121, 2836]; // YES // My algorithm answered NO
 
 function kangaroo(x1, v1, x2, v2) {
-  // console.log(x1, v1, x2, v2);
-  // if (x2 > x1 && v2 > v1) return 'NO';
-
-  let runLoop = true;
   let i = 2;
-  while (runLoop) {
-    let pos1 = x1 + v1 * i;
-    let pos2 = x2 + v2 * i;
-    // console.log(pos1, pos2);
-    if (pos1 === pos2) runLoop = false;
-    console.log(Math.abs(pos1 - pos2));
-    // if (Math.abs(pos1 - pos2) > Math.abs(pos1 + 1 - (pos2 + 1))) return 'NO';
+  while (true) {
+    let p1 = x1 + v1 * i; // p = position
+    let p2 = x2 + v2 * i;
+
+    let nextP1 = x1 + v1 * (i + 1);
+    let nextP2 = x2 + v2 * (i + 1);
+
+    if (p1 === p2) return 'YES';
+    if (Math.abs(p1 - p2) <= Math.abs(nextP1 - nextP2)) return 'NO';
+
     i++;
   }
-
-  if (runLoop === false) return 'YES';
-  // 3*2+0, 3*3+0, 3*4+0, 3*5+0
 }
 
-// console.log(kangaroo(...k1));
+console.log(kangaroo(...k1));
 console.log(kangaroo(...k2));
+console.log(kangaroo(...k3));
+console.log(kangaroo(...k4));
 // kangaroo(...k1);
 // kangaroo(...k2);
