8f7601326223bca0f6eb28ffe2bb7e28e1c6a000
zacaree <zacharem@gmail.com>
2019-09-29 13:18:35 -0500

    Beginning new challenge: Luck Balance

---
+++ Interview Preperation Kit/Greedy Algorithms/Luck Balance.js
@@ -0,0 +1,11 @@
+// The maximum number of important contests Lena can lose.
+const k1 = 3;
+// Each set contains that contest's luck balance and its importance rating.
+// 1. How much luck she'll add to her balance if she loses or how much she'll
+//    lose from her balance if she wins that round.
+// 2. 1 means its an important round, 0 means it's not important.
+const contests1 = [[5, 1], [2, 1], [1, 1], [8, 1], [10, 0], [5, 0]];
+
+function luckBalance(k, contests) {}
+
+console.log(luckBalance(k1, contests1));
